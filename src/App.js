93ca6fa8d31f04c69bb70037bb3d8cf90620e516
Derrick Holleman <derrickholleman@gmail.com>
2022-01-22 11:03:28 -0600

    home page done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import Movies from "./HomePage/HomePage";
+import Movie from "./Movie/Movie";
+import Header from "./Header";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Movies />
+        </Route>
+        <Route exact path="/:movieId">
+          <Movie />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import React from "react";
+import './HomePage.css'
+
+const Movies = () => {
+  const [movies, setMovies] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    async function getMovies() {
+      const moviesRes = await fetch("https://ghibliapi.herokuapp.com/films");
+      const moviesJSON = await moviesRes.json();
+      setMovies(moviesJSON);
+      setLoaded(true);
+    }
+
+    if (movies) {
+      getMovies();
+    }
+
+    return () => setLoaded(false);
+  }, []);
+
+  return (
+    <div className="home-page-container">
+      {loaded &&
+        movies.map((movie) => (
+          <div key={movie.id} className="home-page-movie-container">
+            <div className="movie-basic-info">
+              <h2 className="home-page-movie-title">{movie.title}</h2>
+              <p>{movie.release_date}</p>
+            </div>
+            <div className="home-page-movie-poster">
+              <img src={movie.image} alt={`${movie.title} poster`} />
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Movies;
