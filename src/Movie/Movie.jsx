209b58edb921f01e778271c83b74a6dc3a495166
Derrick Holleman <derrickholleman@gmail.com>
2022-01-22 14:16:51 -0600

    movie page content and styling

--- src/Movie/Movie.jsx
+++ src/Movie/Movie.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./Movie.css";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { getMovies } from "../utils/api";
+import { BsFillArrowLeftSquareFill } from "react-icons/bs";
 
 const Movie = () => {
   const [movies, setMovies] = useState([]);
@@ -33,9 +34,27 @@ const Movie = () => {
             style={{ backgroundImage: `url(${foundMovie.movie_banner})` }}
             className="background"
           ></div>
-
-          <div className="movie-page-wrapper">
-            <h1>{foundMovie.title}</h1>
+          <h1 className="movie-header">
+            {foundMovie.title} ({foundMovie.release_date})
+          </h1>
+          <Link to="/" className="back-arrow">
+            <BsFillArrowLeftSquareFill />
+          </Link>
+          <div className="movie-info-section">
+            <div className="movie-info-left-side">
+              <img
+                src={foundMovie.image}
+                alt={`${foundMovie.title} poster`}
+                className="movie-info-poster"
+              />
+            </div>
+            <div className="movie-info-right-side">
+              <p>Directed by: {foundMovie.director}</p>
+              <p>Running time: {foundMovie.running_time} minutes</p>
+              <p>Original Title: {foundMovie.original_title}</p>
+              <p>Description: {foundMovie.description}</p>
+              <p>Rotten Tomatoes Score: {foundMovie.rt_score}%</p>
+            </div>
           </div>
         </div>
       )}
