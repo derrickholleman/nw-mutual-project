35844717118c002d57f3b6061fe99427e9341a1e
Derrick Holleman <derrickholleman@gmail.com>
2022-01-22 14:32:46 -0600

    responsive styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import HomePage from "./HomePage/HomePage";
 import Movie from "./Movie/Movie";
+import Favorites from "./Favorites/Favorites";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
@@ -12,6 +13,9 @@ function App() {
         </Route>
         <Route exact path="/:movieId">
           <Movie />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
         </Route>
       </Switch>
     </div>
