917612ad290c85119df538fb654d04b7aa538554
Derrick Holleman <derrickholleman@gmail.com>
2022-01-22 11:43:39 -0600

    search working

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
+import SearchSort from "../SearchSort/SearchSort";
 import React from "react";
 import "./HomePage.css";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  const [movies, setMovies] = useState({});
+  const [movies, setMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
   const [loaded, setLoaded] = useState(false);
+  const [isFiltering, setIsFiltering] = useState(false);
 
   useEffect(() => {
     async function getMovies() {
@@ -24,8 +28,15 @@ const HomePage = () => {
   return (
     <div>
       <h1>Find A Ghibli Movie</h1>
+      <SearchSort
+        movies={movies}
+        setFilteredMovies={setFilteredMovies}
+        setIsFiltering={setIsFiltering}
+      />
       <div className="home-page-container">
+        {/* default movie list */}
         {loaded &&
+          !isFiltering &&
           movies.map((movie) => (
             <div key={movie.id} className="home-page-movie-container">
               <div className="movie-basic-info">
@@ -33,7 +44,26 @@ const HomePage = () => {
                 <p>{movie.release_date}</p>
               </div>
               <div className="home-page-movie-poster">
-                <img src={movie.image} alt={`${movie.title} poster`} />
+                <Link to={`/${movie.id}`}>
+                  <img src={movie.image} alt={`${movie.title} poster`} />
+                </Link>
+              </div>
+            </div>
+          ))}
+
+        {/* filtered movie list */}
+        {loaded &&
+          isFiltering &&
+          filteredMovies.map((movie) => (
+            <div key={movie.id} className="home-page-movie-container">
+              <div className="movie-basic-info">
+                <h2 className="home-page-movie-title">{movie.title}</h2>
+                <p>{movie.release_date}</p>
+              </div>
+              <div className="home-page-movie-poster">
+                <Link to={`/${movie.id}`}>
+                  <img src={movie.image} alt={`${movie.title} poster`} />
+                </Link>
               </div>
             </div>
           ))}
--- src/SearchSort/SearchSort.jsx
+++ src/SearchSort/SearchSort.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import "./SearchSort.css";
 
-const SearchSort = () => {
-  return <div></div>;
+const SearchSort = ({ movies, setFilteredMovies, setIsFiltering }) => {
+  const [searchTitle, setSearchTitle] = useState("");
+  const handleChange = (e) => {
+    setSearchTitle(e.target.value);
+    if (searchTitle.length > 0) {
+      setIsFiltering(true);
+    }
+  };
+
+  useEffect(() => {
+    setFilteredMovies(
+      movies.filter((movie) => {
+        let noPuncMovieTitle = movie.title.replace(/['"]+/g, "");
+        return noPuncMovieTitle
+          .toLowerCase()
+          .includes(searchTitle.trim().toLowerCase());
+      })
+    );
+  }, [searchTitle]);
+
+  return (
+    <div className="search-sort-container">
+      <input
+        type="text"
+        placeholder="Search by title..."
+        value={searchTitle}
+        onChange={handleChange}
+      />
+    </div>
+  );
 };
 
 export default SearchSort;
